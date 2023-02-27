ebef3cf7420467de235e8f4c2df77f56b66d02d4
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2023-02-27 16:40:34 -0500

    make heroku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 const dontenv = require("dotenv");
 const discountsRouter = require("./routes/discounts");
@@ -7,6 +8,7 @@ dontenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(discountsRouter);
 
 app.listen(3000, () => {
